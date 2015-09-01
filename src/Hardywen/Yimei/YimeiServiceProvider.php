45099cf812c0920eb9e1fb0165b4dfa1cdc40fd9
hardywen <375480616@qq.com>
2015-09-01 16:27:41 +0800

    support laravel 5

--- src/Hardywen/Yimei/YimeiServiceProvider.php
+++ src/Hardywen/Yimei/YimeiServiceProvider.php
@@ -2,47 +2,49 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class YimeiServiceProvider extends ServiceProvider {
+class YimeiServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('hardywen/yimei');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $source = realpath(__DIR__ . '/../../config/config.php');
+        $this->publishes([$source => config_path('yimei.php')]);
+    }
 
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$app = $this->app;
-		$app['yimei'] = $app->share(function ($app) {
-			return Yimei::instance($app['config']->get('yimei::config'));
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $app = $this->app;
+        $app['yimei'] = $app->share(function ($app) {
+            return Yimei::instance($app['config']['yimei']);
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('yimei');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('yimei');
+    }
 
 }
